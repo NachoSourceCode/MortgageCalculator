84bcb0fed5b142f542b63a3f5416765b0df8894d
Matthew Slone <Matthew.Slone@outlook.com>
2018-08-21 15:33:07 -0400

    Calculate your monthly payments based mortgage amount, the interest rate, and the number of months(Period) in which the full mortgage amount is to be paid off

--- MortgageCalculator/MainWindow.xaml.cs
+++ MortgageCalculator/MainWindow.xaml.cs
@@ -22,11 +22,59 @@ namespace MortgageCalculator
     {
         public MainWindow()
         {
+
             InitializeComponent();
+
         }
+
+        //Varibles
+        static public double AmountBorrowed { get; set; }
+        static public double InterestRate { get; set; }
+        static public int MortgagePeriod { get; set; }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+
+            //Get & Parse values
+            AmountBorrowed = (double)Int32.Parse(Amount.Text);
+
+            //Get Interest rate
+            decimal result;
+            if (Decimal.TryParse(Interest.Text, out result))
+                InterestRate = (double)result;
+            //Get mortgage period
+            MortgagePeriod = Int32.Parse(Period.Text);
+
+            //Calculate mortgage
+            MonthlyPayments.Text = CalcMortgage(AmountBorrowed, InterestRate, MortgagePeriod);
+
+        }
+
+        private string CalcMortgage(double amountBorrowed, double interestRate, int mortgagePeriod)
+        {
+
+            double p = amountBorrowed;
+            double r = ConvertToMonthlyInterest(interestRate);
+            double n = YearsToMonths(mortgagePeriod);
+
+            var c = (decimal)(((r * p) * Math.Pow((1 + r), n)) / 
+                    (Math.Pow((1 + r), n) - 1));
+
+            return ($"${Math.Round(c, MidpointRounding.AwayFromZero)}");
+
+        }
+
+        private int YearsToMonths(int years)
+        {
+
+            return (12 * years);
+
+        }
+
+        private double ConvertToMonthlyInterest(double percent)
+        {
+
+            return (percent / 12) / 100;
 
         }
     }
